050a4fc85b915b5bd4115a5f47cbf85dcf0cf43b
MADHAVAN200 <madhavannadar23@gmail.com>
2026-02-10 11:00:00 +0530

    feat(auth): Add reCAPTCHA v2 and word captcha for bot protection

---
+++ lib/features/auth/auth_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/features/auth/core/login_screen.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LoginScreen();
+  }
+}
+
+// [mod:2026-02-10T11:00:00+05:30]

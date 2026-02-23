2831123dcf8156c90c75a1024233a1abe7b53143
MADHAVAN200 <madhavannadar23@gmail.com>
2026-02-23 09:00:00 +0530

    feat(live-attendance): Add live attendance item model and page

---
+++ lib/features/live_attendance/live_attendance_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/features/live_attendance/views/live_attendance_mobile_portrait_view.dart';
+import 'package:flutter_application/features/live_attendance/views/live_attendance_tablet_portrait_view.dart';
+
+class LiveAttendancePage extends StatelessWidget {
+  const LiveAttendancePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return const MobileLiveAttendanceContent();
+        }
+        return const LiveAttendanceView();
+      },
+    );
+  }
+}
+
+// [mod:2026-02-23T09:00:00+05:30]

ab36787dcbcd6a3f5701c37ce501fd8928655bd9
MADHAVAN200 <madhavannadar23@gmail.com>
2026-02-23 14:00:00 +0530

    feat(collaboration): Add chat models and real-time service layer

---
+++ lib/features/collaboration/collaboration_page.dart
@@ -0,0 +1,19 @@
+﻿import 'package:flutter/material.dart';
+import 'package:flutter_application/shared/layout/responsive_layout.dart';
+import 'package:flutter_application/features/collaboration/views/collaboration_mobile_portrait_view.dart';
+import 'package:flutter_application/features/collaboration/views/collaboration_tablet_portrait_view.dart';
+
+class CollaborationScreen extends StatelessWidget {
+  const CollaborationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ResponsiveLayout(
+      mobile: CollaborationMobileView(),
+      tabletPortrait: CollaborationTabletView(),
+      tabletLandscape: CollaborationTabletView(),
+    );
+  }
+}
+
+// [mod:2026-02-23T14:00:00+05:30]
